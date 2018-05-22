fc99ff676cd33d141408cea5807269627690ec80
Avaer Kazmer <a@modules.io>
2018-05-21 22:20:16 -0400

    Bugfix express static server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const port = parseInt(process.env['PORT'], 10) || 8000;
 
 const app = express();
-app.use('*', express.static(__dirname));
+app.use(express.static(__dirname));
 http.createServer(app)
   .listen(port, () => {
     console.log(`http://127.0.0.1:${port}/`);
